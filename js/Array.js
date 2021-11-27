2ba69603a942ca56422ff14c67abec236c8e4348
Adxell <adxell02@gmail.com>
2021-11-26 21:47:14 -0500

    add js

---
+++ js/Array.js
@@ -0,0 +1,16 @@
+/*Array*/
+/*Create Array*/
+
+let fruits=['Fig','Mango','Plum', 'Banana']; 
+
+console.log(fruits.length);
+//4
+
+let first = fruits[0];
+console.log(first);
+//fig
+
+let last=fruits[fruits.length-1];
+
+console.log(last);
+//Banana
