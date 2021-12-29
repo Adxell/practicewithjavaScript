ff0f6a4f72bf92d449de759cbd1801981badd6b2
Adxell <adxell02@gmail.com>
2021-12-29 16:20:59 -0500

    update Array iterator

--- js/ArrayIterator/Arrayiterator.js
+++ js/ArrayIterator/Arrayiterator.js
@@ -18,17 +18,27 @@
 // console.log(eArr.next().value);
 //----------funtion loop-------------------
 
-function logIterable(objects){
-    if(!(Symbol.iterator in Object.getPrototypeOf(objects))){
-        console.log(objects, 'is not an iterable object...');
-        return;
-    }
-
-    let iterator = objects[Symbol.iterator]();
-    for (let letter of iterator){
-        console.log(letter);
-    }
+let inputText=document.querySelector(".inputText");
+let button = document.querySelector(".buttonAction");
+let buttonTwo = document.querySelector(".buttonActionTwo");
+buttonTwo.addEventListener("click",()=>{
+    button.toggleAttribute("hidden");
+})
+buttonTwo.addEventListener("click", ()=>{
+    logIterable(inputText.value);
+})
+inputText.addEventListener("keydown",()=>{
+    buttonTwo.removeAttribute("disabled");
+})
+function logIterable(objects) {
+  if (!(Symbol.iterator in Object.getPrototypeOf(objects))) {
+    console.log(objects, "is not an iterable object...");
+    return;
+  }
+  let iterator = objects[Symbol.iterator]();
+  for (let letter of iterator) {
+    console.log(letter);
+  }
 }
-logIterable(["a", "b", "c"]);
-logIterable("hola mi nombre es adxell");
+
 
