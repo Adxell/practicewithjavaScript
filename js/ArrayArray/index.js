fa20cdc55a7140e76b358a93ac81dd7d12a3ce87
Adxell <adxell02@gmail.com>
2021-12-29 17:08:07 -0500

    add Array method

---
+++ js/ArrayArray/index.js
@@ -0,0 +1,7 @@
+let nothing=[1,'hello', "compro"];
+console.log(nothing);
+let array= new Array(1,2,"however");
+console.log(array);
+let arrayTwo = new Array(3);
+console.log(arrayTwo.length);
+console.log(arrayTwo);
