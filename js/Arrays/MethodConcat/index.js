a82989edd40172d8e5c140550d5821882a70dcd5
Adxell <adxell02@gmail.com>
2021-12-30 14:36:20 -0500

    add Method Concat

---
+++ js/Arrays/MethodConcat/index.js
@@ -0,0 +1,35 @@
+// const array1=['a','b','c'];
+// const array2=['d','e','f'];
+// const array3=array1.concat(array2);
+// console.log(array3);
+// const array4=["j","h",'k']
+// const result = array1.concat(1,2,3,4,"h",true);
+// console.log(result);
+// console.log(array1)
+
+// const letters = ['a','b','c'];
+// const numbers=[1,2,3];
+
+// const alphaNumeric = letters.concat(numbers);
+// console.log(alphaNumeric)
+
+
+// const num1 = [1, 2, 3];
+// const num2 = [4, 5, 6];
+// const num3 = [7, 8, 9,10,11,12];
+
+// const number4=num1.concat(num2,num3);
+// console.log(number4)
+
+// const letters = ['a','b','c'];
+// const alphaNumeric= letters.concat(1,[2,3]);
+// console.log(alphaNumeric);
+
+// const num1=[[1]];
+// const num2=[2,[3]];
+
+// const numbers=num1.concat(num2);
+// console.log(numbers);
+// num1[0].push(5);
+// num2[1].push(45)
+// console.log(numbers);
