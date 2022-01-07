a9343ef26a586965bb5217557231900b7025e057
Adxell <adxell02@gmail.com>
2022-01-06 22:30:48 -0500

    update Method find index

--- js/Arrays/MethodFindIndex/index.js
+++ js/Arrays/MethodFindIndex/index.js
@@ -4,4 +4,27 @@
 
  console.log(array1.findIndex(isLargerNumber));
 
- 
+ //find the index of a prime in array 
+
+ function isPrime(num){
+     for(let i=2; num > i; i++){
+         if(num % i == 0){
+             return false;
+         }
+     }
+
+     return num > 1; 
+ }
+
+ console.log([4,6,8,9,12].findIndex(isPrime));
+ console.log([4,6,7,9,12].findIndex(isPrime));
+
+
+//find index with arrow function
+
+const fruits = ["apple ", "banana", "cantaloupe", "blueberries", "grapefruit"];
+
+const index = fruits.findIndex((fruit) => fruit === "cantaloupe");
+
+console.log(index)
+console.log(fruits[index])
