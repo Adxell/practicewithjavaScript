d29dcd48c2ac9d9e7626aaf8402c1e1b86e1ca58
Adxell <adxell02@gmail.com>
2022-01-07 20:22:00 -0500

    add method isArray

---
+++ js/Arrays/MethodIsArray/index.js
@@ -0,0 +1,20 @@
+// all following calls return true
+Array.isArray([]);
+Array.isArray([1]);
+Array.isArray(new Array());
+Array.isArray(new Array("a", "b", "c", "d"));
+Array.isArray(new Array(3));
+// Little known fact: Array.prototype itself is an array:
+Array.isArray(Array.prototype);
+
+// all following calls return false
+console.log(Array.isArray());
+console.log(Array.isArray({}));
+console.log(Array.isArray(null));
+console.log(Array.isArray(undefined));
+console.log(Array.isArray(17));
+console.log(Array.isArray("Array"));
+console.log(Array.isArray(true));
+console.log(Array.isArray(false));
+console.log(Array.isArray(new Uint8Array(32)));
+console.log(Array.isArray({ __proto__: Array.prototype }));
