b444b3f5278531441f2bff9837b1d1cae4fd29b5
Adxell <adxell02@gmail.com>
2022-01-07 19:47:22 -0500

    update method

---
+++ js/Arrays/MethodIncludes/index.js
@@ -0,0 +1,20 @@
+// const array1=[1,2,3];
+
+
+// console.log(array1.includes(2))
+
+// const pets = ['cat', 'dog', 'bat'];
+
+// console.log(pets.includes('cat'));
+
+
+// console.log(pets.includes("at"));
+
+//example 
+
+console.log([1,2,3].includes(2));
+console.log([1,2,3].includes(4));
+console.log([1,2,3].includes(3,3));
+console.log([1,2,3].includes(3,-1));
+console.log([1,2,NaN].includes(NaN));
+console.log(["1","2","3"].includes(3));
