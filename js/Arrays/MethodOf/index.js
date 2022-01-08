efcb5536b31d59f9f2cf4e41773db5a30fbbae0b
Adxell <adxell02@gmail.com>
2022-01-08 15:51:39 -0500

    add method Arrayf

---
+++ js/Arrays/MethodOf/index.js
@@ -0,0 +1,6 @@
+console.log(Array.of(7,8,9,9,0,0,2,132))
+console.log(Array.of(undefined))
+//examples
+Array.of(1);         // [1]
+Array.of(1, 2, 3);   // [1, 2, 3]
+Array.of(undefined); // [undefined]
