351b490282b5e8f82808c326040ce70e2c3c6713
Adxell <adxell02@gmail.com>
2022-01-11 21:19:54 -0500

    add method reverse

---
+++ js/Arrays/MethodReverse/index.js
@@ -0,0 +1,24 @@
+const array1 = ['one', 'two', 'three'];
+
+
+console.log('array1: ', array1)
+
+
+const reversed = array1.reverse();
+console.log('Reverse: ', reversed)
+
+console.log('array1: ', array1)
+
+//example 
+const arr = [1, 2, 3]; 
+
+console.log(arr)
+
+console.log(arr.reverse())
+
+//Reversing the elements in an array-like
+
+
+const a = {0:1, 1:2, 2:3, length: 3};
+
+console.log(a)
