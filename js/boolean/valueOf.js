436b9399f32925a200442c8956b4a100dcd7b04b
Adxell <adxell02@gmail.com>
2022-01-17 14:16:51 -0500

    update boolean

---
+++ js/boolean/valueOf.js
@@ -0,0 +1,13 @@
+const x = new Boolean();
+
+console.log(x.valueOf().toString());
+
+const y = new Boolean('Mozilla');
+
+console.log(y.valueOf());
+
+//Examples 
+
+z = new Boolean();
+myVar = z.valueOf();
+console.log(myVar)
