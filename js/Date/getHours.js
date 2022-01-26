ed01ca3a3acc05def391781ef588513b187256a5
Adxell <adxell02@gmail.com>
2022-01-25 22:41:33 -0500

    add method getHours

---
+++ js/Date/getHours.js
@@ -0,0 +1,9 @@
+const birthday = new Date('March 13, 08 20:20');
+
+console.log(birthday.getHours())
+
+//Examples 
+let Xmas95 = new Date('December 25, 1995 23:15:30');
+let hours = Xmas95.getHours();
+
+console.log(hours)
