2d83076b9ccc8dc4aabe6fede36696ecb4c81e25
Adxell <adxell02@gmail.com>
2022-01-25 22:23:23 -0500

    Add method getDate

---
+++ js/Date/getDate.js
@@ -0,0 +1,11 @@
+const birthday = new Date(1590757517834);
+const date1 = birthday.getDate();
+
+console.log(date1);
+
+//Examples 
+
+let Xmas95 = new Date('December 25, 1995 23:15:30');
+let day = Xmas95.getDate();
+
+console.log(day)
