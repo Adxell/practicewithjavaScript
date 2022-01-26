674d9a0418a95c6a89ddbcdf7f2b3e8138d2d4f4
Adxell <adxell02@gmail.com>
2022-01-25 22:37:39 -0500

    add method getFullYear

---
+++ js/Date/getFullYear.js
@@ -0,0 +1,10 @@
+const moonLanding = new Date('July 20, 69 00:20:18');
+
+console.log(moonLanding.getFullYear());
+
+//Examples
+
+var today = new Date();
+var year = today.getFullYear();
+
+console.log(year)
