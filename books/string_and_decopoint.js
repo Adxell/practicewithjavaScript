92ab8e4b3e68fa3e72b80c85552276e37ee4330b
Adxell <adxell02@gmail.com>
2022-01-28 12:34:01 -0500

    add string

---
+++ books/string_and_decopoint.js
@@ -0,0 +1,30 @@
+let love = "❤";
+let name = "Adxell"
+
+for(let i = 0; i <name.length; i++){
+    console.log(name[i])
+}
+
+for(let letter of name){
+    console.log(letter)
+}
+
+
+console.log(`"She said 'hi'", 
+hi said`)
+console.log('You\'re right, it can\'t be a quote')
+
+console.log('\u{1f600}')
+console.log("\u{03c0}");
+
+let s = 'Hello, worldH';
+
+console.log(s.charCodeAt(12))
+
+console.log("x".padEnd(4, "*"))
+
+
+console.log('\u2718')
+console.log(typeof(null))
+console.log(null)
+console.log(typeof(undefined))
