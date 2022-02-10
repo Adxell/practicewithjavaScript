4f3ad9de5a47b6695c2e5d2c730fdeafd6da67da
Adxell <adxell02@gmail.com>
2022-02-09 22:50:40 -0500

    update eval.js

--- books/eval.js
+++ books/eval.js
@@ -1,1 +1,18 @@
+
+console.clear()
 console.log(eval("3+2"))
+console.log(eval(1+"2"))
+
+
+//Miscellaneous operators
+
+let options = {timeout : 0, title: "", verbose: false, n: null}
+
+console.log(options.timeout??1000)
+console.log(options.title??"untitled")
+console.log(options.verbose??true)
+
+
+//typeof 
+let  value = 14586
+console.log((typeof value === "string")? "'" + value + "'" : value.toString());
