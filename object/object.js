7a06eb461d4823e653abfd7cb9c6220d2d02b0a8
Adxell <adxell02@gmail.com>
2022-02-21 23:44:55 -0500

    update object

--- object/object.js
+++ object/object.js
@@ -27,3 +27,22 @@ let a = new Array();
 let d = new Date();
 
 let r = new Map();
+
+//Object.create()
+
+let o1 = Object.create({x: 1, y: 2});
+
+console.log(o1.x)
+//if i need create a new Object with the properties, is necesary pass Object.prototype 
+
+let o3 = Object.create(Object.prototype);
+console.log(o3)
+
+//Querying and setting proterties 
+
+let author = book.author; 
+
+let name = author["surname"]; 
+console.log(name)
+let title = book["main title"];
+
